4a33157ed75811fe15e263b68fb85244fb004ba0
felix-lhz <1175906605@qq.com>
2024-06-07 09:04:42 +0800

    无向切换拓扑完成

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ if __name__ == '__main__':
     camera = Camera(0)
     camera.start()
     
-    controller_25 = Controller("192.168.137.124", 12345, 25) # DDH的车
+    controller_25 = Controller("192.168.137.219", 12345, 25) # DDH的车
     # controller_22 = Controller("192.168.137.135", 12345, 22) # LK的车
-    controller_24 = Controller("192.168.137.227", 12345, 24) # LHZ的车
-    controller_35 = Controller("192.168.137.56", 12345, 35) # JQH的车
-    controllers = [controller_25, controller_24, controller_35]
+    controller_24 = Controller("192.168.137.143", 12345, 24) # LHZ的车
+    controller_18 = Controller("192.168.137.202", 12345, 18) # JQH的车
+    controllers = [controller_25, controller_24, controller_18]
     for controller in controllers:
         controller.start()
     # 等待初始化
@@ -23,13 +23,13 @@ if __name__ == '__main__':
     #创建一个以controller_id为邻居的无向切换拓扑图
     #25->24/35
     controller_25.add_neighbor(controller_24)
-    controller_25.add_neighbor(controller_35)
+    controller_25.add_neighbor(controller_18)
     #24->35/25
-    controller_24.add_neighbor(controller_35)
+    controller_24.add_neighbor(controller_18)
     controller_24.add_neighbor(controller_25)
     #35->25/24
-    controller_35.add_neighbor(controller_25)
-    controller_35.add_neighbor(controller_24)
+    controller_18.add_neighbor(controller_25)
+    controller_18.add_neighbor(controller_24)
 
     # 创建一个新的图形和子图
     fig, ax = plt.subplots()
@@ -52,7 +52,7 @@ if __name__ == '__main__':
         #计算时间差
         time_diff = current_time - start_time
         #如果时间差大于2.5s，重新规划路径
-        if time_diff > 2.5:
+        if time_diff > 2:
             neighbor_num = (neighbor_num + 1) % 2   
 
         for car in cars:
@@ -67,14 +67,22 @@ if __name__ == '__main__':
                 car_y = car["y"]
                 x = 0
                 y = 0
+                distance = 0
                 nerghbors = controller.get_neighbors()
                 for other_car in cars:
+                    if other_car["id"] != controller.id:
+                        temp_distance = (other_car["x"] - car_x)**2 + (other_car["y"] - car_y)**2
+                        if temp_distance < distance or distance == 0:
+                            distance = temp_distance
                     if other_car["id"] == nerghbors[neighbor_num].id:
                         x = other_car["x"] - car_x
                         y = other_car["y"] - car_y
                 # 设置目标位置
-                target_x = car_x + x
-                target_y = car_y + y
+                target_x = car_x
+                target_y = car_y
+                if distance >100:
+                    target_x = car_x + x
+                    target_y = car_y + y
                 controller.set_position(car_x, car_y, car["theta"])
                 controller.set_target(target_x, target_y)
                 # 绘制小车的位置和目标方向
